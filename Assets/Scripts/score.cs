2b1d6a3335444ab8779a90a14f3dddbdfd30ad86
joshazking <josh@hi9.io>
2019-07-29 18:22:05 +0100

    Revert "added win state and win scene when pickups reach set int"
    This reverts commit 3bea5e2407deed6af8bbd5576d55b5cef5d5d0fd.

--- Assets/Scripts/score.cs
+++ Assets/Scripts/score.cs
@@ -1,18 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
 
 public class score : MonoBehaviour
 {
-    void Update() 
-    {
-        if (points == 10)
-        {
-            SceneManager.LoadScene ("WinScene");
-        }        
-    }
-
     public int points = 0;
 
     private void OnGUI() 
