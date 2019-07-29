3bea5e2407deed6af8bbd5576d55b5cef5d5d0fd
joshazking <josh@hi9.io>
2019-07-29 17:53:54 +0100

    added win state and win scene when pickups reach set int

--- Assets/Scripts/score.cs
+++ Assets/Scripts/score.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class score : MonoBehaviour
 {
+    void Update() 
+    {
+        if (points == 10)
+        {
+            SceneManager.LoadScene ("WinScene");
+        }        
+    }
+
     public int points = 0;
 
     private void OnGUI() 
