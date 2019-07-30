cbcdd050d4a98d001be74cf8a729613770fa1250
joshazking <josh@hi9.io>
2019-07-30 17:55:50 +0100

    added win state again

--- Assets/Scripts/score.cs
+++ Assets/Scripts/score.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class score : MonoBehaviour
 {
@@ -9,6 +10,14 @@ public class score : MonoBehaviour
     private void OnGUI() 
     {
         GUI.Label(new Rect(10,10,1000,200), "Score : " + points);    
+    }
+
+    void Update() 
+    {
+        if (points == 10)
+        {
+            SceneManager.LoadScene("WinScene");
+        } 
     }
 
 }
